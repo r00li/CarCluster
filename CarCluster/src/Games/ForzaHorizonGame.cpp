28cca598dd50fd54a90aaf22d5c178154e615315
Andrej Rolih <roli.r00li@gmail.com>
2024-09-13 22:17:22 +0200

    Modified Forza protocol to support FM2023

--- CarCluster/src/Games/ForzaHorizonGame.cpp
+++ CarCluster/src/Games/ForzaHorizonGame.cpp
@@ -13,11 +13,13 @@ ForzaHorizonGame::ForzaHorizonGame(GameState& game, int port): Game(game) {
 void ForzaHorizonGame::begin() {
   // Forza (Horizon) sends data as a UDP blob of data
   // Telemetry protocol described here: https://medium.com/@makvoid/building-a-digital-dashboard-for-forza-using-python-62a0358cb43b
+  // FM2023 protocol described here: https://github.com/r00li/CarCluster/pull/7
 
   if (forzaUdp.listen(port)) {
     forzaUdp.onPacket([this](AsyncUDPPacket packet) {
-      if (packet.length() == 324) {
+      if (packet.length() == 324 || packet.length() == 331) {
         char dataBuff[4];  // four bytes in a float 32
+        bool isFM2023Format = packet.length() == 331;
 
         // CURRENT_ENGINE_RPM
         memcpy(dataBuff, (packet.data() + 16), 4);
@@ -44,13 +46,15 @@ void ForzaHorizonGame::begin() {
         }
 
         // SPEED
-        memcpy(dataBuff, (packet.data() + 256), 4);
+        int speedMemoryOffset = isFM2023Format ? 244 : 256;
+        memcpy(dataBuff, (packet.data() + speedMemoryOffset), 4);
         int someSpeed = *((float*)dataBuff);
         someSpeed = someSpeed * 3.6;
         gameState.speed = someSpeed;
 
         // GEAR
-        memcpy(dataBuff, (packet.data() + 319), 1);
+        int gearMemoryOffset = isFM2023Format ? 307 : 319;
+        memcpy(dataBuff, (packet.data() + gearMemoryOffset), 1);
         int forzaGear = (int)(dataBuff[0]);
         if (forzaGear == 0) {
           gameState.gear = GearState_Auto_R;
